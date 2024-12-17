c8050d5735031eb586f3b55d46ce467ade49a42a
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2024-12-17 10:05:19 +0100

    connect to db

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,38 @@
 import express, { Application } from 'express';
-import 'dotenv/config'
-import connectDB from './db';
+import 'dotenv/config';
+import connectDB from './db/index';
+
+class App {
+  private app: Application;
+  private port: number;
+
+  constructor() {
+    this.app = express();
+    this.port = Number(process.env.PORT) || 3000; // Преобразуем порт к числу
+    this.initDB();
+    this.initMiddleware();
+    this.startServer();
+  }
+
+  private async initDB(): Promise<void> {
+    try {
+      await connectDB();
+      console.log('Database connected successfully');
+    } catch (error) {
+      console.error('Database connection failed: ', error);
+    }
+  }
+
+  private initMiddleware(): void {
+    this.app.use(express.json()); 
+  }
+
+  private startServer(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on http://localhost:${this.port}`);
+    });
+  }
+}
+
+
+new App();
